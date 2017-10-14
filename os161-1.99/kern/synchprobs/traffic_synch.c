847eadfca86f43b34098a28f66643857fecb38c4
aaronchazh <avchazho@student.cs.uwaterloo.ca>
2017-10-14 09:35:47 -0400

    adding a1p2

--- os161-1.99/kern/synchprobs/traffic_synch.c
+++ os161-1.99/kern/synchprobs/traffic_synch.c
@@ -3,10 +3,11 @@
 #include <synchprobs.h>
 #include <synch.h>
 #include <opt-A1.h>
+#include <array.h>
 
 /* 
  * This simple default synchronization mechanism allows only vehicle at a time
- * into the intersection.   The intersectionSem is used as a a lock.
+ * into the intersection.   The nsSem is used as a a lock.
  * We use a semaphore rather than a lock so that this code will work even
  * before locks are implemented.
  */
@@ -21,8 +22,62 @@
 /*
  * replace this with declarations of any synchronization and other variables you need here
  */
-static struct semaphore *intersectionSem;
+static struct lock *ilock;
+static struct cv *icv;
+static struct array *cars;
 
+typedef struct car {
+  Direction origin;
+  Direction dest;
+} car;
+
+int allowed(car *first, car *inside);
+bool right_turn(car* c);
+// bool left_turn(car* v);
+void wait(car *c);
+
+bool right_turn(car* c) {
+  if (((c->origin == west) && (c->dest == south)) ||
+      ((c->origin == east) && (c->dest == north)) ||
+      ((c->origin == south) && (c->dest == east))  ||
+      ((c->origin == north) && (c->dest == west))) {
+    return true;
+  }
+  return false;
+}
+
+// bool left_turn(car* v) {
+//   if (((v->origin == west) && (v->dest == north)) ||
+//       ((v->origin == east) && (v->dest == south)) ||
+//       ((v->origin == south) && (v->dest == west)) ||
+//       ((v->origin == north) && (v->dest == east))) {
+//     return true;
+//   }
+//   return false;
+// }
+
+int allowed(car* first, car* inside) {
+  if (first->origin == inside->origin) {
+    return 1;
+  }
+  else if ((first->origin == inside->dest) && (first->dest == inside->origin)) {
+    return 1;
+  }
+  else if ((right_turn(first) || right_turn(inside)) && (first->dest != inside->dest)) {
+    return 1;
+  }
+  // else if (left_turn(first) && left_turn(inside)) {
+  //   if ((first->dest == east || first->dest == west) &&
+  //       (inside->dest == east || inside->dest == west)) {
+  //     return 1;
+  //   }
+  //   else if ((first->dest == north || first->dest == south) &&
+  //           (inside->dest == north|| inside->dest == south)) {
+  //     return 1;
+  //   }
+  // }
+  return 0;
+}
 
 /* 
  * The simulation driver will call this function once before starting
@@ -36,11 +91,18 @@ intersection_sync_init(void)
 {
   /* replace this default implementation with your own implementation */
 
-  intersectionSem = sem_create("intersectionSem",1);
-  if (intersectionSem == NULL) {
-    panic("could not create intersection semaphore");
+  ilock = lock_create("ilock");
+  if (ilock == NULL) {
+    panic("could not create intersection lock");
   }
-  return;
+  icv = cv_create("icv");
+  if (icv == NULL) {
+    panic("could not create intersection condition variable");
+  }
+  cars = array_create();
+  if (cars == NULL) {
+    panic("could not create intersection cars queue");
+  }
 }
 
 /* 
@@ -50,14 +112,29 @@ intersection_sync_init(void)
  * You can use it to clean up any synchronization and other variables.
  *
  */
+
+void wait (car *c) {
+  int size = array_num(cars);
+  for (int i = 0; i < size; i++) {
+    if (!allowed(c, array_get(cars, i))) {
+      cv_wait(icv, ilock);
+      return wait(c);
+    }
+  }  
+  array_add(cars, c, NULL);
+}
+
 void
 intersection_sync_cleanup(void)
 {
   /* replace this default implementation with your own implementation */
-  KASSERT(intersectionSem != NULL);
-  sem_destroy(intersectionSem);
+  KASSERT(ilock != NULL);
+  lock_destroy(ilock);
+  KASSERT(icv != NULL);
+  cv_destroy(icv);
+  KASSERT(cars != NULL);
+  array_destroy(cars);
 }
-
 
 /*
  * The simulation driver will call this function each time a vehicle
@@ -76,10 +153,23 @@ void
 intersection_before_entry(Direction origin, Direction destination) 
 {
   /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  P(intersectionSem);
+  
+  lock_acquire(ilock);
+  car *first = kmalloc(sizeof(struct car));
+  first->origin = origin;
+  first->dest = destination;
+
+  // int size = array_num(cars);
+  // for (int i = 0; i < size; i++) {
+  //   if (!allowed(first, array_get(cars, i))) {
+  //     cv_wait(icv, ilock);
+  //     continue;
+  //   }
+  // }
+  // array_add(cars, first, NULL);
+
+  wait(first);
+  lock_release(ilock);
 }
 
 
@@ -98,8 +188,18 @@ void
 intersection_after_exit(Direction origin, Direction destination) 
 {
   /* replace this default implementation with your own implementation */
-  (void)origin;  /* avoid compiler complaint about unused parameter */
-  (void)destination; /* avoid compiler complaint about unused parameter */
-  KASSERT(intersectionSem != NULL);
-  V(intersectionSem);
+  
+  lock_acquire(ilock);
+  int size = array_num(cars);
+  car *ith = NULL;
+  for (int i = 0; i < size; i++) {
+    ith = array_get(cars, i);
+    if ((ith->origin == origin) && (ith->dest == destination)) {
+      array_remove(cars, i);
+      cv_broadcast(icv, ilock);
+      break;
+    }
+  }
+  lock_release(ilock);
 }
+
--- os161-1.99/kern/thread/synch.c
+++ os161-1.99/kern/thread/synch.c
@@ -300,21 +300,17 @@ void
 cv_signal(struct cv *cv, struct lock *lock)
 {
         // Write this
-    if (lock_do_i_hold(lock)) {
-        wchan_wakeone(cv->cv_wchan);
-    }
+    wchan_wakeone(cv->cv_wchan);
 	//(void)cv;    // suppress warning until code gets written
-	//(void)lock;  // suppress warning until code gets written
+	(void)lock;  // suppress warning until code gets written
 }
 
 void
 cv_broadcast(struct cv *cv, struct lock *lock)
 {
 	// Write this
-    if (lock_do_i_hold(lock)) {
-        wchan_wakeall(cv->cv_wchan);
-    }
+    wchan_wakeall(cv->cv_wchan);
 	//(void)cv;    // suppress warning until code gets written
-	//(void)lock;  // suppress warning until code gets written
+	(void)lock;  // suppress warning until code gets written
 }
 
